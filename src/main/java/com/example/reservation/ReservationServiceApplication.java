13b3e200a34ba03d881a3c8b3f17d024cd8fc73f
mahima <dodmahima@gmail.com>
2025-08-30 16:55:23 +0530

    initial commit

--- src/main/java/com/example/reservation/ReservationServiceApplication.java
+++ src/main/java/com/example/reservation/ReservationServiceApplication.java
@@ -3,11 +3,13 @@ package com.example.reservation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @EnableR2dbcRepositories(basePackages = "com.example.reservation.repository")
 @SpringBootApplication
 @EnableCaching
+@EnableDiscoveryClient
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
